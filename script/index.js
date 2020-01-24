01a983677ea36e6a2c962a952823dd6dcbb26503
Vladimir <79533330011@ya.ru>
2020-01-24 18:18:46 +0300

    lesson02easy

--- script/index.js
+++ script/index.js
@@ -1,10 +1,20 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 30000;
+let income = 10000;
+let addExpenses = '1500, 400, 2000, 800, 2500';
+let deposit = false;
+let mission = 100000;
+let period = 6;
+let budgetDay = (money+income) / 30;
 
-alert('Закрыть вопрос по закапыванию юзера?');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-console.log('Мать земля, помоги мне!');
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+console.log(addExpenses.toLowerCase().split(','));
+
+console.log(budgetDay);
