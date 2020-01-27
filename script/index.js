9229220d541a0283b1177ee70e2cce34a98e5911
Vladimir <79533330011@ya.ru>
2020-01-27 16:03:03 +0300

    Третий урок завершен

--- script/index.js
+++ script/index.js
@@ -1,20 +1,53 @@
-let money = 30000;
-let income = 10000;
-let addExpenses = '1500, 400, 2000, 800, 2500';
-let deposit = false;
-let mission = 100000;
-let period = 6;
-let budgetDay = (money+income) / 30;
+document.addEventListener('DOMContentLoaded', () => {
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+    let money = +prompt('Ваш месячный доход?');
+    let income = +10000;
+    let expensesName1 = prompt('Введите название обязатальной статьи расходов в этом месяце');
+    let expenses1 = +prompt('Введите расходуемую на ' + expensesName1 + ' сумму');
+    let expensesName2 = prompt('Введите название обязатальной статьи расходов в этом месяце');
+    let expenses2 = +prompt('Введите расходуемую на ' + expensesName2 + ' сумму');
+    let addExpenses = prompt('Перечислите через запятую названия дополнительных статей расходов в этом месяце');
+    // function arraySum(addExpenses) {
+    //     addExpenses = prompt('Перечислите через запятую ваши возможные расходы за рассчитываемый период').split(',');
+    //     var sum = 0;
+    //     for(let i = 0; i < addExpenses.length; i++){
+    //         sum += addExpenses[i]*1;
+    //         }
+    //     return sum;
+    //     }
 
-console.log(addExpenses.length);
+    let deposit = confirm('Есть ли у вас депоозит в банке?');
+    let mission = 100000;
+    let period = 6;
+    let budgetMonth = money + income - expenses1 - expenses2;
+    let budgetDay = budgetMonth / 30;
+    console.log(budgetMonth);
+    console.log(budgetDay);
+    let numMonth = Math.ceil(100000 / budgetMonth);
+    console.log(numMonth);
 
-console.log('Период равен ' + period + ' месяцев');
-console.log('Цель заработать ' + mission + ' рублей');
 
-console.log(addExpenses.toLowerCase().split(','));
+    console.log(typeof money);
+    console.log(typeof income);
+    console.log(typeof deposit);
 
-console.log(budgetDay);
+
+    console.log('Период равен ' + period + ' месяцев');
+    console.log('Цель заработать ' + mission + ' рублей');
+    console.log('Цель будет достигнута за ' + numMonth + ' месяцев');
+    console.log('Бюджет на день составит ' + budgetDay + ' рублей');
+
+
+    if(budgetDay >= 1200) {
+        console.log('У вас высокий уровень дохода');
+    } else if (budgetDay < 1200 && budgetDay >= 600) {
+        console.log('У вас средний уровень дохода');
+    } else if (budgetDay < 600) {
+        console.log('Ваш уровень дохода ниже среднего');
+    } else if (budgetDay <= 0) {
+        console.log('Что-то пошло не так');
+    }
+
+    console.log(addExpenses.toLowerCase().split(','))
+
+});
