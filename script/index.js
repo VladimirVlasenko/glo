4fd781ad855fee53191ace536284e4eacb950ee6
Vladimir <79533330011@ya.ru>
2020-02-04 15:13:40 +0300

    основное задание к 7му уроку

--- script/index.js
+++ script/index.js
@@ -1,106 +1,80 @@
+let money;
 
-    let isNumber = function(n){
-        return !isNaN(parseFloat(n)) && isFinite(n);
-    }
-     
-     let money;
+let isNumber = function(n){
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
 
+let start = function() {
+    do {money = prompt('Ваш месячный доход?', 50000)}
+    while(!isNumber(money));
+    return +money;
+};
 
-    let start = function() {
-        do {money = prompt('Ваш месячный доход?', 50000)}
-        while(!isNumber(money));
-        return +money;
-    };
+start();
 
-    start();
+let appData = {
+    budget: money,
+    income: {},
+    addIncome: [],
+    expenses: {},
+    addExpenses: [],
+    deposit: false,
+    mission: 50000,
+    period: 3,
+    asking: function() {
+        let addExpenses = prompt('Перечислите через запятую названия ' +  
+            'дополнительных статей расходов в этом месяце' +
+            '!','вода, газ, свет, инет, телефон');
+            appData.addExpenses = addExpenses.toLowerCase().split(',');
+            appData.deposit = confirm('Есть ли у вас депозит в банке?');
 
-    let income = +10000;
-    let addExpenses = prompt('Перечислите через запятую названия дополнительных статей расходов в этом месяце', 'вода, газ, свет, инет, телефон');
-
-
-   
-
-    let deposit = confirm('Есть ли у вас депозит в банке?');
-    let mission = +100000;
-    let period = 6;
-
-   
-
-    let showTypeOf = function(data) {
-        console.log(data, typeof(data));
-    }
-    showTypeOf(money);
-    showTypeOf(income);
-    showTypeOf(deposit);
-
-    let expenses1,
-        expenses2;
-
-
-    
-    let getExpensesMonth = function() {
-        
-        let sum;
-        let sum1 = 0;
         for (let i = 0; i < 2; i++) {
-
-            if (i === 0) {
-                expenses1 = prompt('Введите название обязатальной статьи расходов в этом месяце', 'Адвокааааат!');
-            } else if(i === 1) {
-                expenses2 = prompt('Введите название обязатальной статьи расходов в этом месяце', 'Шиномонтаж!');
-            }
-
-            do {
-                sum = prompt('Во сколько это обойдется?', 3000);
-            }
-            while (!isNumber(sum));
-            sum1 += +sum;
+                appData.expenses[prompt('Введите название обязатальной статьи расходов в этом месяце' + (i), 'Адвокааааат!'+ (i) )] = +prompt('Во сколько это обойдется?', 3000);
         }
-        return sum1;
-    }
-
-    let expensesMonth = getExpensesMonth();
-
-    console.log(expensesMonth + ' - сумма всех обязательных расходов за месяц');
-
-    let getAccumulatedMonth = function() {
-        return +money + income - +expensesMonth;
-    }
-
-    let accumulatedMonth = getAccumulatedMonth();
-    console.log(accumulatedMonth + ' - накопления за месяц');
-
-    let budgetDay = accumulatedMonth / 30;
-
-    let getTargetMonth = function() {
-        return Math.ceil(mission / accumulatedMonth);
-    }
-
-
-
-    console.log('Период равен ' + period + ' месяцев');
-    console.log('Цель заработать ' + mission + ' рублей');
-
-    if (getTargetMonth() < 0) {
-        console.log('Цель не будет достигнута')
-    } else {
-        console.log('Цель будет достигнута за ' + getTargetMonth() + ' месяцев');
-    }
-
-    console.log('Бюджет на день составит ' + budgetDay + ' рублей');
-
-    let getStatusIncome = function() {
-        if(budgetDay >= 1200) {
+    },
+    budgetDay: 0,
+    budgetMonth: 0,
+    expensesMonth: 0,
+    getExpensesMonth: function() {
+        let sum = 0;
+        for(key in appData.expenses) {
+            sum += +appData.expenses[key];
+            }    
+        console.log(sum + 'р - расходы за месяц');
+        appData.expensesMonth = +sum;
+    },
+    getBudget: function() {
+        appData.budgetMonth = +money - +appData.expensesMonth;
+        appData.budgetDay = appData.budgetMonth / 30;
+    },
+    getTargetMonth: function() {
+        return Math.ceil(appData.mission / appData.budgetMonth);
+    },
+    getStatusIncome: function() {
+        if(appData.budgetDay >= 1200) {
             return ('У вас высокий уровень дохода');
-        } else if (budgetDay < 1200 && budgetDay >= 600) {
+        } else if (appData.budgetDay < 1200 && appData.budgetDay >= 600) {
             return ('У вас средний уровень дохода');
-        } else if (budgetDay < 600) {
+        } else if (appData.budgetDay < 600) {
             return ('Ваш уровень дохода ниже среднего');
-        } else if (budgetDay <= 0) {
+        } else if (appData.budgetDay <= 0) {
             return ('Что-то пошло не так');
         }
-    };
+    }
+};
 
-    console.log(getStatusIncome());
+appData.asking();
+appData.getExpensesMonth();
+appData.getBudget();
 
-    console.log(addExpenses.toLowerCase().split(','));
+if (appData.getTargetMonth() < 0) {
+    console.log('Цель не будет достигнута')
+} else {
+    console.log('Цель будет достигнута за ' + (Math.ceil(appData.mission / appData.budgetMonth)) + ' месяцев');
+};
+
+console.log(appData.getStatusIncome());
+
+for(let key in appData) {
+    console.log('Наша программа включает в себя следующие данные: ' + key + " : " + appData[key]);
+}
